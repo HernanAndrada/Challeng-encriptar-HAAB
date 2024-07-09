595342bf4b692f9d875adf7fa3a7d5c4e65cbd49
Hernan Andrada <hernanandrada277@gmail.com>
2024-07-09 02:38:26 -0300

    proyecto terminado

---
+++ challenge/encriptar.js
@@ -0,0 +1,21 @@
+function encryptText(text) {
+    let encryptedText = text
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+    
+    return encryptedText;
+}
+
+function decryptText(text) {
+    let decryptedText = text
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+    
+    return decryptedText;
+}
